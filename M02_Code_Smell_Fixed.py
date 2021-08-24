98caac98cbc3a22ae426851576d6be3351d8e705
KinguBread <89031894+KinguBread@users.noreply.github.com>
2021-08-24 15:07:59 -0400

    Delete M02_Code_Smell_Fixed.py

--- M02_Code_Smell_Fixed.py
+++
@@ -1,6 +0,0 @@
-uncle_sam = 1.08
-pay_the_server = 1.15
-food_stuff = float(input("How much was your meal? "))
-corprate_cut = uncle_sam * food_stuff
-pocket_book_pain = corprate_cut * pay_the_server
-print("You need to leave " + str(pocket_book_pain) + " on the table to cover tax and tip.")
