3e6dfcbfa591c851ca3943c02fc3bee99944e850
KinguBread <i1andersoni1@yahoo.com>
2021-08-24 14:48:38 -0400

    If Statment

---
+++ If_statment.py
@@ -0,0 +1,7 @@
+num = int(input("Please give me a number, 5 or 6. "))
+num1 = 5
+num2 = 6
+if num == num1:
+    print("Hello")
+if num == num2:
+    print("Goodbye")
--- Numbers.py
+++ Numbers.py
@@ -1,5 +1,6 @@
-num1 = 5
+num1 = 5.5
 num2 = 12
 num3 = num1 + num2
-print(num3)
+#print(num3)
+#print(type(num3))
 print("My number is " + str(num3))
