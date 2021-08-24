f0f8178f7adbeca32ab06497d46c591fd4d5df40
KinguBread <i1andersoni1@yahoo.com>
2021-08-24 14:48:38 -0400

    Debugged.

---
+++ Temp_conversion.py
@@ -0,0 +1,4 @@
+num = int(input("Please tell me what tempature it is outside in Fehrenheit "))
+num1 = (num - 32/1.8)
+print("It is currently " + str(num) + " degrees Fehrenheit outside or " + str(num1) + " degrese Celecius.")
+#print("Your new tempature in Celecius is. " + str(num1))
