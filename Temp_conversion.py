4003f3917fb45d3c392728c2c78cdef9cd8e4467
KinguBread <89031894+KinguBread@users.noreply.github.com>
2021-08-24 15:08:04 -0400

    Delete Temp_conversion.py

--- Temp_conversion.py
+++
@@ -1,4 +0,0 @@
-num = int(input("Please tell me what tempature it is outside in Fehrenheit "))
-num1 = (num - 32/1.8)
-print("It is currently " + str(num) + " degrees Fehrenheit outside or " + str(num1) + " degrese Celecius.")
-#print("Your new tempature in Celecius is. " + str(num1))
