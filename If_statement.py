bbfca947e360ba218a0aecf9608c7d99486649e3
KinguBread <i1andersoni1@yahoo.com>
2021-08-25 11:04:13 -0400

    Rebuilding

---
+++ If_statement.py
@@ -0,0 +1,7 @@
+num = int(input("Pick a number, 5 or 6 \n"))
+num1 = 5
+num2 = 6
+if num == num1:
+    print("Hello")
+if num == num2:
+    print("Goodbye")
---
+++ Temp_conversion.py
@@ -0,0 +1,3 @@
+num = int(input("What is the current temparture outside in Fehrenheit? "))
+num1 = (num - 32/1.8)
+print("The current tempature outside in Fehrenheit is " + str(num) + " this converted to Celecius is " + str(num1))
